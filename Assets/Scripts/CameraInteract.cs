f70a57d29fb5cfc3cdb4e8f91593e801231cf224
Kraaven <dhruv.bhojaraj@gmail.com>
2024-02-08 17:32:48 +0530

    update 4
    -Finished basic Game

--- Assets/Scripts/CameraInteract.cs
+++ Assets/Scripts/CameraInteract.cs
@@ -2,15 +2,27 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CameraInteract : MonoBehaviour
 {
     private boxScript Tbox;
     public GameObject Ball;
+    public List<boxScript> Boxes;
+
+    private Vector3 BallInitPos;
+    private Quaternion BallInitRot;
+
+    private void Awake()
+    {
+        Boxes = new List<boxScript>();
+    }
 
     private void Start()
     {
         Tbox = FindObjectOfType<boxScript>();
+        BallInitPos = Ball.transform.position;
+        BallInitRot = Ball.transform.rotation;
     }
 
     void Update()
@@ -18,7 +30,7 @@ public class CameraInteract : MonoBehaviour
         RaycastHit hit;
         Ray R = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        if (Physics.Raycast(R, out hit, 40))
+        if (Physics.Raycast(R, out hit, 150))
         {
             if (hit.transform.TryGetComponent(out boxScript box))
             {
@@ -56,7 +68,25 @@ public class CameraInteract : MonoBehaviour
             {
                 Vector3 forceDirection = hit.point  - Ball.transform.position;
                 Ball.GetComponent<Rigidbody>().AddForce(forceDirection.normalized * 2, ForceMode.Impulse);
+            }
+        }
 
+        if (Input.GetMouseButton(1))
+        {
+            if (Tbox != null)
+            {
+                Ball.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            foreach (var box in Boxes)
+            {
+                box.resetPos();
+                Ball.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                Ball.transform.position = BallInitPos;
+                Ball.transform.rotation = BallInitRot;
             }
         }
     }
--- Assets/Scripts/boxScript.cs
+++ Assets/Scripts/boxScript.cs
@@ -1,8 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class boxScript : MonoBehaviour
 {
+    public Vector3 initpos;
+    private void Start()
+    {
+        initpos = transform.position;
+        FindObjectOfType<CameraInteract>().Boxes.Add(this);
+    }
 
+    public void resetPos()
+    {
+        gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        transform.rotation = Quaternion.Euler(-90,0,0);
+        transform.position = initpos;
+        
+        
+    }
 }
